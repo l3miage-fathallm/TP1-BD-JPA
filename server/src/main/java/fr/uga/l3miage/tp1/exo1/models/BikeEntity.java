63819ec743d0691d07d0a2e8f2a5f12a62e65bbc
l3miage-fathallm <mariam.fathallah@etu.univ-grenoble-alpes.fr>
2024-03-12 15:37:59 +0100

    creation de l'entité BikeEntity (exo 1.5)

---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -0,0 +1,26 @@
+package fr.uga.l3miage.tp1.exo1.models;
+
+import javax.persistence.*;
+import fr.uga.l3miage.tp1.exo1.enums.ShifterType;
+import fr.uga.l3miage.tp1.exo1.enums.CylinderNumber;
+import java.time.LocalDate;
+
+public class BikeEntity {
+    @Id
+    @Column(name = "immatriculation")
+    private String immatriculation;
+    @Column(name = "capacity")
+    private Integer capacity;
+    @Column(name= "cylinder_number")
+    @Enumerated(EnumType.ORDINAL)
+    private CylinderNumber cylinderNumber;
+    @Column(name = "automatic")
+    private boolean automatic;
+    @Column(name = "shifter")
+    private boolean shifter;
+    @Column(name = "shifter_type")
+    @Enumerated(EnumType.STRING)
+    private ShifterType shifterType;
+    @Column(name = "circulation_date")
+    private LocalDate circulationDate;
+}
