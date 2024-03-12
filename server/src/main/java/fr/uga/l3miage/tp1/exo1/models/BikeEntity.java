38e681efea9c205167f341c4fab0927d985c06c6
l3miage-fathallm <mariam.fathallah@etu.univ-grenoble-alpes.fr>
2024-03-12 16:35:17 +0100

    modifications BikeEntity.java

--- server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -5,6 +5,8 @@ import fr.uga.l3miage.tp1.exo1.enums.ShifterType;
 import fr.uga.l3miage.tp1.exo1.enums.CylinderNumber;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class BikeEntity {
     @Id
     @Column(name = "immatriculation")
@@ -12,14 +14,14 @@ public class BikeEntity {
     @Column(name = "capacity")
     private Integer capacity;
     @Column(name= "cylinder_number")
-    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
     private CylinderNumber cylinderNumber;
     @Column(name = "automatic")
     private boolean automatic;
     @Column(name = "shifter")
     private boolean shifter;
     @Column(name = "shifter_type")
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.ORDINAL)
     private ShifterType shifterType;
     @Column(name = "circulation_date")
     private LocalDate circulationDate;
